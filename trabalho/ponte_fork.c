ec8311e58a17669c91ca1ef35d24f502c5fca722
Lucas <lucaspulsz@gmail.com>
2024-06-24 14:50:00 -0300

    Implementando o uso se semaforo para garantir que um processo seja executado por vez

--- trabalho/ponte_fork.c
+++ trabalho/ponte_fork.c
@@ -3,9 +3,14 @@
 #include <time.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #define TAMANHO 50
-#define CARROS_POR_MINUTO 60 // 30-60-90 veículos por minuto
+#define CARROS_POR_MINUTO 90 // 30-60-90 veículos por minuto
 #define TRUE 1
 #define FALSE 0
 
@@ -17,26 +22,36 @@ struct carro {
 };
 typedef struct carro CARRO;
 
-void ponte(CARRO *carro) {
+void ponte(CARRO *carro, sem_t *semaforo) {
+    sem_wait(semaforo); // trava semaforo
     printf("PONTE 1\tCarro = %i\t Prioridade = %i\t Processo = %d\n", carro->id, carro->prioridade, getpid());
     usleep(60000000 / CARROS_POR_MINUTO); // delay para limitar velocidade de carros que passam na ponte
+    sem_post(semaforo); // libera semaforo
 }
 
-void ponte2(CARRO *carro2) {
+void ponte2(CARRO *carro2, sem_t *semaforo) {
+    sem_wait(semaforo); // trava semaforo
     printf("PONTE 2\tCarro2 = %i\t Prioridade = %i\t Processo = %d\n", carro2->id, carro2->prioridade, getpid());
     usleep(60000000 / CARROS_POR_MINUTO); // delay para limitar velocidade de carros que passam na ponte
+    sem_post(semaforo); // libera semaforo
 }
 
 int main(void) {
     // Inicia gerador de números aleatórios
     srand(time(NULL));
-    
-    CARRO *carros = NULL;
-    CARRO *carros2 = NULL;
 
-    // Alocação de memória para os parâmetros
-    carros = (CARRO *)malloc(TAMANHO * sizeof(CARRO));
-    carros2 = (CARRO *)malloc(TAMANHO * sizeof(CARRO));
+    int shmid, shmid2;
+    CARRO *carros, *carros2;
+
+    // Criação da memória compartilhada
+    shmid = shmget(IPC_PRIVATE, TAMANHO * sizeof(CARRO), IPC_CREAT | 0600);
+    carros = (CARRO *)shmat(shmid, NULL, 0);
+
+    shmid2 = shmget(IPC_PRIVATE, TAMANHO * sizeof(CARRO), IPC_CREAT | 0600);
+    carros2 = (CARRO *)shmat(shmid2, NULL, 0);
+
+    // Criação do semáforo
+    sem_t *semaforo = sem_open("/semaforo_ponte", O_CREAT, 0600, 1);
 
     // Criando carros com suas prioridades
     for (int i = 0; i < TAMANHO; i++) {
@@ -48,56 +63,25 @@ int main(void) {
         carros2[i].prioridade = (rand() % 5) + 1;
         carros2[i].atravessou_ponte = FALSE;
     }
-    
+
     // Executa a função de acordo com a prioridade
     for (int prio = 1; prio <= 5; prio++) {
         for (int i = 0; i < TAMANHO; i++) {
-            if (carros[i].prioridade == prio) {
+            if (carros[i].prioridade == prio && carros[i].atravessou_ponte == FALSE) {
                 carros[i].pid = fork();
                 if (carros[i].pid == 0) {
-                    ponte(&carros[i]);
-                    exit(0);
-                } else if (carros[i].pid > 0) {
+                    ponte(&carros[i], semaforo);
                     carros[i].atravessou_ponte = TRUE;
+                    shmdt(carros);
                 }
             }
-            if (carros2[i].prioridade == prio) {
+            if (carros2[i].prioridade == prio && carros2[i].atravessou_ponte == FALSE) {
                 carros2[i].pid = fork();
                 if (carros2[i].pid == 0) {
-                    ponte2(&carros2[i]);
-                    exit(0);
-                } else if (carros2[i].pid > 0) {
+                    ponte2(&carros2[i], semaforo);
                     carros2[i].atravessou_ponte = TRUE;
+                    shmdt(carros2);
                 }
-            }
-
-            int sinaleira =  (rand() % (TAMANHO * 3)) + 1;
-            if (sinaleira == 7) {
-                // Faz o wait para garantir que todos os processos acabaram antes de parar
-                for (int j = 0; j < TAMANHO; j++) {
-                    if (carros[j].atravessou_ponte == TRUE) {
-                        waitpid(carros[j].pid, NULL, 0);
-                    }
-                    if (carros2[j].atravessou_ponte == TRUE) {
-                        waitpid(carros2[j].pid, NULL, 0);
-                    }
-                }
-
-                printf("\nSinaleira Fechada!\nCarros Aguardando Sinaleira:\n");
-                printf("PONTE 1\n");
-                for (int j = 0; j < TAMANHO; j++) {
-                    if (carros[j].atravessou_ponte == FALSE) {
-                        printf("\t*(Carro = %i - Prioridade = %i)\n", carros[j].id, carros[j].prioridade);
-                    }
-                }
-                printf("PONTE 2\n");
-                for (int j = 0; j < TAMANHO; j++) {
-                    if (carros2[j].atravessou_ponte == FALSE) {
-                        printf("\t*(Carro = %i - Prioridade = %i)\n", carros2[j].id, carros2[j].prioridade);
-                    }
-                }
-                sleep(10);
-                printf("\nSinaleira Aberta!\n");
             }
         }
     }
@@ -112,8 +96,14 @@ int main(void) {
         }
     }
 
-    free(carros);
-    free(carros2);
+    // Liberação da memória compartilhada e do semáforo
+    shmdt(carros);
+    shmctl(shmid, IPC_RMID, NULL);
+    shmdt(carros2);
+    shmctl(shmid2, IPC_RMID, NULL);
+
+    sem_close(semaforo);
+    sem_unlink("/semaforo_ponte");
 
     return 0;
 }
